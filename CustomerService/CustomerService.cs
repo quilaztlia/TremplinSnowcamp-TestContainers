ee114e8a99dbebe1db21525efb73638de5a6629c
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-11-02 01:19:09 +0100

    Initial project

---
+++ CustomerService/CustomerService.cs
@@ -0,0 +1,64 @@
+﻿using CustomerService;
+
+namespace Customers;
+
+public sealed class CustomerService
+{
+    private readonly DbConnectionProvider _dbConnectionProvider;
+
+    public CustomerService(DbConnectionProvider dbConnectionProvider)
+    {
+        _dbConnectionProvider = dbConnectionProvider;
+        CreateCustomersTable();
+    }
+
+    public IEnumerable<Customer> GetCustomers(){
+        IList<Customer> customers = new List<Customer>();
+
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+
+        command.CommandText = "select id, name from Customers";
+        command.Connection?.Open();
+
+        using var dataReader = command.ExecuteReader();
+        while(dataReader.Read()){
+            var id = dataReader.GetInt64(0);
+            var name = dataReader.GetString(1);
+            customers.Add(new Customer(id, name));
+        }
+        return customers;
+    }
+
+    public void Create(Customer customer){
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+
+        var id = command.CreateParameter();
+        id.ParameterName = "@id";
+        id.Value = customer.Id;
+
+        var name = command.CreateParameter();
+        name.ParameterName = "@name";
+        name.Value = customer.Name;
+
+        command.CommandText="insert into Customers(id, name) values(@id, @name)";
+        command.Parameters.Add(id);
+        command.Parameters.Add(name);
+        command.Connection?.Open();
+        command.ExecuteNonQuery();
+    }
+    
+    private void CreateCustomersTable()
+    {
+        using var connection = _dbConnectionProvider.GetConnection();
+        using var command = connection.CreateCommand();
+        command.CommandText = "create table if not exists "+ 
+         "Customers(id bigint not null "+
+                  ", name varchar not null "+
+                  ", Primary Key(id))";
+        command.Connection?.Open();
+        command.ExecuteNonQuery();
+    }
+
+}
