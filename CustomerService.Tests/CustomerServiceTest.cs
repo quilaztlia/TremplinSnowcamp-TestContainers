b88bcd5af8a02f20ce828570cc88624f97f2d839
Adriana NAVA AGUILAR <a.nava-aguilar.ext@funecap.com>
2023-12-15 13:02:21 +0100

    Slides

--- CustomerService.Tests/CustomerServiceTest.cs
+++ CustomerService.Tests/CustomerServiceTest.cs
@@ -1,20 +1,42 @@
 using System.Reflection;
 using System.Runtime.Intrinsics.Arm;
 using CustomerService;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using Testcontainers.PostgreSql;
 
 namespace Customers.Tests;
 
-public sealed class CustomerServiceTest
+public sealed class CustomerServiceTest : IAsyncLifetime
 {    
   //"postgres:15-alpine"
-    [Fact]
-    public void ShouldFindTwoCustomersAfterCreateTwoCustomers() {
-      //Arrange
+  private readonly PostgreSqlContainer _db = new PostgreSqlBuilder()
+  .WithImage("postgres:15-alpine")
+  .Build();   
 
-      //Act Insert 3 Customers :     
-      //Act: Get Customers are found                  
+    public async Task InitializeAsync()
+    {
+        await _db.StartAsync();
+    }
+
+  public async Task DisposeAsync()
+    {
+        await _db.DisposeAsync();
+    }
+    
+    [Fact]    
+    public void ShouldFindTwoCustomersAfterCreateTwoCustomers() {
+      //Arrange     
+      var service = new CustomerService(new DbConnectionProvider(_db.GetConnectionString()));
+
+      //Act Insert 3 Customers :  
+      service.Create(new Customer(1, "Criteo"));   
+      service.Create(new Customer(2, "Snwocap"));
+      service.Create(new Customer(3, "CraftsRecords"));
+
+      //Act: Get Customers are found   
+      var customers = service.GetCustomers();
 
       //Assert: 
+      Assert.Equal(3, customers.Count());
     }
 }
--- CustomerService/CustomerService.cs
+++ CustomerService/CustomerService.cs
@@ -11,7 +11,7 @@ public sealed class CustomerService
         _dbConnectionProvider = dbConnectionProvider;
         CreateCustomersTable();
     }
-
+   
     private void CreateCustomersTable()
     {
         using var connection = _dbConnectionProvider.GetConnection();
